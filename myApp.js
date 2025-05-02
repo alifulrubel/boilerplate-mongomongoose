da343e1e33924f9ad97b79e7022d3212504630b6
aliful-islam <aliful.islam@orbitax.com>
2025-05-02 15:47:03 +0600

    Initial commit with my solution

--- myApp.js
+++ myApp.js
@@ -1,4 +1,17 @@
-require('dotenv').config();
+require('dotenv').config();  // Load environment variables from .env
+const mongoose = require('mongoose');  // Import mongoose
+
+// Connect to MongoDB Atlas using the MONGO_URI from .env
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Connected to MongoDB Atlas!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB: ", err);
+  });
 
 
 let Person;
