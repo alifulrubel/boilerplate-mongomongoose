881ac6bede4925947924a4467bb1fe94f25bc14d
aliful-islam <aliful.islam@orbitax.com>
2025-06-09 16:50:44 +0600

    fp 6

--- myApp.js
+++ myApp.js
@@ -45,82 +45,50 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  Person.find({ name: personName }, (err, data) => {
+Person.find({ name: personName }, (err, data) => {
     if (err) return done(err);
     done(null, data);
-  });
 };
 
-
 const findOneByFood = (food, done) => {
-  Person.findOne({ favoriteFoods: food }, (err, data) => {
-    if (err) return done(err);
-    done(null, data);
-  });
+  done(null /*, data*/);
 };
 
 const findPersonById = (personId, done) => {
-  Person.findById(personId, (err, data) => {
-    if (err) return done(err);
-    done(null, data);
-  });
+  done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  Person.findById(personId, (err, person) => {
-    if (err) return done(err);
-    person.favoriteFoods.push(foodToAdd);
-    person.save((err, updatedPerson) => {
-      if (err) return done(err);
-      done(null, updatedPerson);
-    });
-  });
+  done(null /*, data*/);
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  Person.findOneAndUpdate(
-    { name: personName },
-    { age: ageToSet },
-    { new: true },
-    (err, updatedDoc) => {
-      if (err) return done(err);
-      done(null, updatedDoc);
-    }
-  );
+  done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
-  Person.findByIdAndRemove(personId, (err, deletedDoc) => {
-    if (err) return done(err);
-    done(null, deletedDoc);
-  });
+  done(null /*, data*/);
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  Person.deleteMany({ name: nameToRemove }, (err, result) => {
-    if (err) return done(err);
-    done(null, result);
-  });
+  done(null /*, data*/);
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find({ favoriteFoods: foodToSearch })
-    .sort({ name: 1 })         // sort by name ascending
-    .limit(2)                  // limit to 2 results
-    .select('-age')            // exclude age field
-    .exec((err, data) => {
-      if (err) return done(err);
-      done(null, data);
-    });
+  done(null /*, data*/);
 };
+
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
